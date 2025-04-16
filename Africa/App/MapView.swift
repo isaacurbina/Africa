0513a79169b90911bc8b3b64fa9e1bc911cc91c7
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-15 19:46:03 -0500

    95. 1. Let's set up the iOS project in Xcode (#1)

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Isaac Urbina on 4/15/25.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+#Preview {
+    MapView()
+}

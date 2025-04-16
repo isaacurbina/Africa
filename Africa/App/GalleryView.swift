5b16af61efe33899c7ac2ae3e01439a0f6c24030
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-15 19:44:56 -0500

    95. 1. Let's set up the iOS project in Xcode

---
+++ Africa/App/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  Africa
+//
+//  Created by Isaac Urbina on 4/15/25.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Gallery")
+    }
+}
+
+#Preview {
+    GalleryView()
+}
---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Isaac Urbina on 4/15/25.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Video")
+    }
+}
+
+#Preview {
+    VideoListView()
+}

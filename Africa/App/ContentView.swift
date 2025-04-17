f89bc5b6be9c9b8f494d90e35af61736f9ed3bc1
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-17 17:06:43 -0500

    98. 4. Let's develop the detail view for each animal with dynamic data - Part 1

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -25,14 +25,21 @@ struct ContentView: View {
 					.listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
 				
 				ForEach(animals) { animal in
-					AnimalListItemView(animal: animal)
-				}
+					NavigationLink(destination: AnimalDetailView(animal: animal)) {
+						AnimalListItemView(animal: animal)
+					} // NavigationLink
+				} // ForEach
 			} // List
 			.navigationBarTitle("Africa", displayMode: .large)
 		} // NavigationView
     }
 }
 
-#Preview {
-    ContentView()
+
+// MARK: - preview
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
 }

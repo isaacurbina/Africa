bccac3346f57753707b5882a423f36b45779e0ac
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-16 19:02:00 -0500

    97. 3. Let's create a dynamic SwiftUI List using Swift Generics and JSON (#3)

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
 	
 	// MARK: - properties
 	
+	let animals: [Animal] = Bundle.main.decode("animals.json")
 	
 	
 	// MARK: - body
@@ -22,6 +23,10 @@ struct ContentView: View {
 				CoverImageView()
 					.frame(height: 300)
 					.listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+				
+				ForEach(animals) { animal in
+					AnimalListItemView(animal: animal)
+				}
 			} // List
 			.navigationBarTitle("Africa", displayMode: .large)
 		} // NavigationView

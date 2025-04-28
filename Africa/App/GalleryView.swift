25929f421bf475649a1284f5b712533182dbe34c
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-28 14:01:35 -0500

    105. 11. Let's create a photo gallery with Grid Layout using SwiftUI (#11)

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,14 +8,92 @@
 import SwiftUI
 
 struct GalleryView: View {
+	
+	
+	// MARK: - properties
+	
+	@State private var selectedAnimal: String = "lion"
+	let animals: [Animal] = Bundle.main.decode("animals.json")
+	let haptics = UIImpactFeedbackGenerator(style: .medium)
+	// simple grid definition
+//	let gridLayout: [GridItem] = [
+//		GridItem(.flexible()),
+//		GridItem(.flexible()),
+//		GridItem(.flexible())
+//	]
+	
+	
+	// efficient grid definition
+//	let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+	
+	// dynamic grid layout
+	@State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+	@State private var gridColumn: Double = 3.0
+	
+	
+	// MARK: - functions
+	
+	func gridSwitch() {
+		gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+	}
+	
+	
+	// MARK: - body
+	
     var body: some View {
 		ScrollView(.vertical, showsIndicators: false) {
-			Text("Gallery")
+			VStack(alignment: .center, spacing: 30) {
+				
+				
+				// MARK: - image
+				
+				Image(selectedAnimal)
+					.resizable()
+					.scaledToFit()
+					.clipShape(Circle())
+					.overlay(Circle().stroke(Color.white, lineWidth: 8))
+				
+				
+				// MARK: - slider
+				
+				Slider(value: $gridColumn, in : 2...4, step: 1)
+					.padding(.horizontal)
+					.onChange(of: gridColumn) {
+						gridSwitch()
+					}
+				
+				
+				// MARK: - grid
+				
+				LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+					ForEach(animals) { item in
+						Image(item.image)
+							.resizable()
+							.scaledToFit()
+							.clipShape(Circle())
+							.overlay(Circle().stroke(Color.white, lineWidth: 1))
+							.onTapGesture {
+								selectedAnimal = item.image
+								haptics.impactOccurred()
+							}
+					} // ForEach
+				} // LazyVGrid
+				.animation(.easeIn)
+				.onAppear(perform: {
+					gridSwitch()
+				})
+			} // VStack
+			.padding(.horizontal, 10)
+			.padding(.vertical, 50)
+			
 		} // ScrollView
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
 		.background(MotionAnimationView())
     }
 }
+
+
+// MARK: - preview
 
 #Preview {
     GalleryView()
